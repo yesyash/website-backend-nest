ab7b792e8743329759aeea5496a0c3df50a6b09c
yash raj <yashraj0207@outlook.com>
2024-10-15 21:28:30 +0530

    create a custom validator

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { validate } from './env.validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate,
+      cache: true,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/env.validation.ts
@@ -0,0 +1,34 @@
+import { plainToInstance } from 'class-transformer';
+import { IsEnum, IsNumber, Max, Min, validateSync } from 'class-validator';
+
+enum EnvironmentEnum {
+  DEVELOPMENT = 'development',
+  PRODUCTION = 'production',
+  STAGING = 'staging',
+}
+
+class EnvironmentVariables {
+  @IsEnum(EnvironmentEnum)
+  ENV: EnvironmentEnum;
+
+  @IsNumber()
+  @Min(0)
+  @Max(65535)
+  PORT: number;
+}
+
+export function validate(config: Record<string, unknown>) {
+  const validatedConfig = plainToInstance(EnvironmentVariables, config, {
+    enableImplicitConversion: true,
+  });
+
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+
+  return validatedConfig;
+}
