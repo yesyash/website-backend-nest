344b761d9f4e22d8984432ce79da1b36fe91488a
yash raj <yashraj0207@outlook.com>
2024-10-16 00:09:17 +0530

    create users module & install typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApiConfigService } from './api-config.service';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import githubConfig from './config/github.config';
 import commonConfig from './config/common.config';
+import githubConfig from './config/github.config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -13,8 +13,19 @@ import commonConfig from './config/common.config';
       isGlobal: true,
       load: [commonConfig, githubConfig],
     }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'testuser',
+      password: 'testpassword',
+      database: 'website-backend',
+      ssl: false, // TODO: this is only for development, set this to false in prod
+      autoLoadEntities: true,
+    }),
+    UsersModule,
   ],
-  controllers: [AppController],
-  providers: [ApiConfigService, AppService],
+  controllers: [],
+  providers: [ApiConfigService],
 })
 export class AppModule {}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getAllUsers(): Promise<User[]> {
+    return this.usersService.getAllUsers();
+  }
+}
